80b915fe54ac857d2e6ee07374c7672ef39495c1
tanjoin <tanjoin@users.noreply.github.com>
2018-09-17 00:15:07 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function main() {
+  var target = document.getElementById('actions-modal');
+  if (!target) {
+    window.setTimeout(main, 500);
+    return;
+  }
+  var observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutationRecord) => {
+    setTimeout(() => {
+        var modal = document.getElementById('actions-modal');
+        if (modal.style.display === 'block') {
+          document.querySelector("#actions-modal > div > div.cf.padding-b--20.padding-r--20.padding-t--5 > div > button.js-action-button.js-retweet-button.Button--primary").click();
+          modal.style.display = 'none';
+        }
+     }, 1);
+    });
+  });
+  var target = document.getElementById('actions-modal');
+  observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+}
+main();
