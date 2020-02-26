20b16fe6c85c2ce9d905adb8f692117894a820e5
tanjoin <tanjoin@users.noreply.github.com>
2020-02-26 10:11:30 +0900

    Ver. 0.1.6

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-function main() {
+function autoRT() {
   var target = document.getElementById('actions-modal');
   if (!target) {
-    window.setTimeout(main, 500);
+    console.log(`actions-modal is not found.`);
     return;
   }
   var observer = new MutationObserver((mutations) => {
@@ -14,5 +14,44 @@ function main() {
     });
   });
   observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+}
+
+// ref: https://github.com/eramdam/BetterTweetDeck/pull/324
+function keepHashTags() {
+  const tweetTextArea = document.querySelector('textarea.js-compose-text');
+  if (!tweetTextArea) {
+    console.log(`textarea.js-compose-text is not found.`);
+    return;
+  }
+  let hashTags = [];
+  const tweetObserver = new MutationObserver(() => {
+    if (tweetTextArea.disabled) {
+      const tweetText = tweetTextArea.value;
+      const tweetedHashtags = window.twttrTxt.extractHashtags(tweetText);
+      hashtags = tweetedHashtags;
+    } else {
+      if (hashtags.length !== 0) {
+        tweetTextArea.value = ` ${hashtags.map(t => `#${t}`).join(' ')}`;
+      }
+      tweetTextArea.focus();
+      tweetTextArea.selectionStart = 0;
+      tweetTextArea.selectionEnd = 0;
+      tweetTextArea.dispatchEvent(new Event('change'));
+    }
+  });
+  tweetObserver.observe(tweetTextArea, {
+    attributes: true,
+    attributeFilter: ['disabled']
+  });
+}
+
+function main() {
+  const keepHashtagsScript = document.createElement('script');
+  keepHashtagsScript.appendChild(document.createTextNode(`(${keepHashTags.toString()})()`));
+  document.body.appendChild(keepHashtagsScript);
+
+  const autoRTScript = document.createElement('script');
+  autoRTScript.appendChild(document.createTextNode(`(${autoRT.toString()})()`));
+  document.body.appendChild(autoRTScript);
 }
 main();
